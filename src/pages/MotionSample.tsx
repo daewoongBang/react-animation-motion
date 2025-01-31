332d8b7b9c52afd6ff9175119341493d284e8ff0
Daewoong Bang <daewoong.bang@gmail.com>
2025-02-01 03:58:23 +0900

    Add Layout Animation Sample

--- src/pages/MotionSample.tsx
+++ src/pages/MotionSample.tsx
@@ -1,10 +1,12 @@
-import MotionSvg from '../components/motion/Svg';
+import MotionLayoutAnimation from '../components/motion/LayoutAnimation';
+// import MotionAnimatePresence from '../components/motion/AnimatePresence';
+// import MotionSvg from '../components/motion/Svg';
 // import MotionValues from '../components/motion/Values';
 // import MotionVariants from '../components/motion/Variants';
 // import MotionGestures from '../components/motion/Gestures';
 
 const MotionSample = () => {
-  return <MotionSvg />;
+  return <MotionLayoutAnimation />;
 };
 
 export default MotionSample;
