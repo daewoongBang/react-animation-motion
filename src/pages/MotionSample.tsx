ed549edfa597e17f932ccb81cffc0a19331fa5d1
Daewoong Bang <daewoong.bang@gmail.com>
2025-01-31 23:12:49 +0900

    Add svg sample

--- src/pages/MotionSample.tsx
+++ src/pages/MotionSample.tsx
@@ -1,9 +1,10 @@
-import MotionValues from '../components/motion/Values';
+import MotionSvg from '../components/motion/Svg';
+// import MotionValues from '../components/motion/Values';
 // import MotionVariants from '../components/motion/Variants';
 // import MotionGestures from '../components/motion/Gestures';
 
 const MotionSample = () => {
-  return <MotionValues />;
+  return <MotionSvg />;
 };
 
 export default MotionSample;
